24f05c50a71196f2d28fa4034ff9d2ff13fc6baf
ShadowDreamer <ShadowDreamer@ShadowDreamerdeMac-mini.local>
2014-07-15 04:23:14 +0800

    Initial Commit

---
+++ CrazyBounce/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CrazyBounce
+//
+//  Created by ShadowDreamer on 7/15/14.
+//  Copyright (c) 2014 ShadowDreamer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
